8e873549135c95fea452b351f0794e2781e41172
tzhouam <tzhouam@connect.ust.hk>
2026-05-21 15:51:21 +0000

    rebase: align vllm-omni with vLLM unknown
    Signed-off-by: tzhouam <tzhouam@connect.ust.hk>

--- vllm_omni/diffusion/diffusion_engine.py
+++ vllm_omni/diffusion/diffusion_engine.py
@@ -896,8 +896,7 @@ class DiffusionEngine:
                 worker_thread.join(timeout=10)
             if worker_thread.is_alive():
                 logger.warning(
-                    "Worker thread did not terminate within 10s; "
-                    "scheduler and executor shutdown will be deferred."
+                    "Worker thread did not terminate within 10s; scheduler and executor shutdown will be deferred."
                 )
                 return
             else:
